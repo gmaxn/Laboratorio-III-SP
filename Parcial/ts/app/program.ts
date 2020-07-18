64c037930bc59550f86feb6b522289b66bf95903
gmaxn <maxi.ng@hotmail.com>
2020-07-17 21:54:27 -0300

    clean main

--- Parcial/ts/app/program.ts
+++ Parcial/ts/app/program.ts
@@ -12,43 +12,12 @@ export default class Program
     static main(...args: string[]): void
     {
         Program.initialize();
+
+
         
-        // BEGIN Test
-        // const grid = new PetGrid(
-        //     new ColBinder("Nombre", "nombre"),
-        //     new ColBinder("Atributos", "vidas", "tipo", "raza")
-        // );
-
-        // var gato = new Gato("Tom", 7);
-        // var perro = new Perro("Chacho", "Salchicha");
-        // var pajaro = new Pajaro("Pako", eTipo.Rapiña);
-
-        // grid.Add(gato);
-        // grid.Add(perro);
-        // grid.Add(pajaro);
-
-        // let table = grid.toGrid();
-
-
-        // let wrapper = document.getElementById("table-wrapper");
-
-        // wrapper.appendChild(table);
-        // END Test
     }
 
     static initialize(): void {
-
-        // const petGrid = new PetGrid(
-        //     new ColBinder("Nombre", "nombre"),
-        //     new ColBinder("Atributos", "vidas", "tipo", "raza")
-        // );
-
-        // let button = document.getElementById("new-pet");
-        // let search = document.getElementById("search");
-        // search.addEventListener("keyup", (e) => petGrid.search(e));
-        // button.addEventListener("click", () => petGrid.generate());
-        // let searchbar = document.getElementById("search-bar");
-
 
         const vehicleGrid = new VehicleGrid(
             new ColBinder("Id", "_id"),
